67a3519ebb7ef83a8ea99a83fbcaad3a36186fd7
dhoang <dhoangk07@gmail.com>
2018-03-01 16:01:41 +0700

    Provide association between User and Comment.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180228140834) do
+ActiveRecord::Schema.define(version: 20180301080544) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20180228140834) do
     t.bigint "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
