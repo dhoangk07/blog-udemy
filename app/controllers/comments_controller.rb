37ceb33ea089edb1e7701de97ea47af13d151ee0
dhoang <dhoangk07@gmail.com>
2018-03-02 16:52:32 +0700

    Fix bug

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,9 @@
 class CommentsController < ApplicationController
+	def show
+		@article = Article.find(params[:id])
+		@comment = @article.comments.find(params[:id])
+	end
+
 	def create
 		@article = Article.find(params[:article_id])
 		@comment = @article.comments.create(comment_params)
@@ -6,13 +11,13 @@ class CommentsController < ApplicationController
 	end
 	def destroy
 		@article = Article.find(params[:article_id])
-		@comment = @article.comments.find(params[:id])
+		@comment = Comment.all.where(:article => params[:id]).first
 		@comment.destroy
-		redirect_to article_path(@article)
+		redirect_to article_path
 	end
 	
 	private
 		def comment_params
-			params.require(:comment).permit(:commenter, :body, :user_id)
+			params.require(:comment).permit(:commenter, :body, :user_id, :article_id)
 		end
 end
